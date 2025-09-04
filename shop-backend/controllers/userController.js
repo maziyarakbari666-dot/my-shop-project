d5845f4596682399b4a5cbe5e79798eaa33cfec0
maziyarakbari666-dot <maziyarakbari666@gmail.com>
2025-09-03 22:09:31 -0700

    Add user controller

---
+++ shop-backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../user');
+
+// ثبت نام کاربر جدید
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({ message: 'User created successfully', user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// دریافت لیست کاربران
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
