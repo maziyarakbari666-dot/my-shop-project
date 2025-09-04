4bd29fa97ac7a0ac113629f49e5d29046a79022a
maziyarakbari666-dot <maziyarakbari666@gmail.com>
2025-09-04 02:56:38 -0700

    بارگذاری پروژه کامل فرانت و بک‌اند

--- shop-backend/models/product.js
+++ shop-backend/models/product.js
@@ -2,17 +2,11 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: String,
-  image: String,
   price: { type: Number, required: true },
-  inventory: { type: Number, required: true, default: 0 },
-  isAvailable: { type: Boolean, default: true },
-  unavailableTimes: [{
-    start: String, // e.g. '10:00'
-    end: String    // e.g. '12:00'
-  }],
-  category: { type: String },
-  createdAt: { type: Date, default: Date.now }
-});
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  image: { type: String },
+  description: { type: String },
+  stock: { type: Number, default: 0 },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
